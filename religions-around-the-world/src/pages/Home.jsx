7496515f98d4a52067865ae879e48b2abe14fd85
spiderasasen <spiderasasen@gmail.com>
2026-01-23 16:19:38 -0500

    the main idea of religion is added to home. but now, i have to add sql. something i finally get to get back too since i was pushing it away for a minute

--- religions-around-the-world/src/pages/Home.jsx
+++ religions-around-the-world/src/pages/Home.jsx
@@ -1,10 +1,22 @@
+import {Link} from 'react-router-dom'
+import religions from "../data/religions.json"
+
 function Home(){
     // a list of religions will be placed, here. making the transition to religion page more simple
     return(
-        <>
+        <div>
             <h1>Religious Encyclopedia</h1>
             <h3>Please chose a Religion</h3>
-        </>
+            <ul>
+                {
+                    religions.map((rel) =>(
+                        <li key={rel.id}>
+                            <Link to={`/religions/${rel.id}`}>{rel.name}</Link>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
     );
 }
 export default Home;
